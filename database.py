9ec65b2de168d5d82a80802db6ced1a4187fc53e
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-07-16 22:15:40 +0000

    end of code, asking user if they want to see the data they inputted

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from os import system, name
+from os import system
 from time import sleep
 
 def clear():
@@ -9,6 +9,7 @@ def clear():
 
 
 questions = True
+print_data = True
 user_desc = {}
 
 
@@ -22,7 +23,7 @@ class Users:
 
 while questions is True:
     print("Tell me about yourself")
-    user_name = input("What is your name?\n").title()
+    name = input("What is your name?\n").lower()
     age = int(input("How old are you?\n"))
     hobby = input("What is your favourite hobby?\n").lower()
     user_desc[name] = Users(name, age, hobby)
@@ -43,17 +44,25 @@ while questions is True:
         clear()
         questions = True
     elif extra_user == "N":
-        print("Okay, thank you for inputting your data")
+        print("Okay, thank you for inputting your data...")
+        sleep(0.7)
+        clear()
         break
-print("Would you like to see the user data that has been inputted? (Y/N)")
-q = input()
-u = q.lower()
-if q not in ("Y", "N"):
-    print("Please enter Y/N")
-    print("Would you like to see the user data that has been inputted? (Y/N)")
-if q == "Y":
+print(user_desc.keys())
+while print_data is True:
+    print("Who's user data would you like to see? (Input users name)")
+    u = input()
+    u = u.replace("'", "")
+    u = u.lower()
     print(f"Your name is {u.title()}, You are {user_desc[u].age} years old, and your favourite hobby is {user_desc[u].hobby}")
-elif q == "N":
-    print("Okay, thank you for inputting your data, we look forward to stealing your identity:)")
-    sleep(1)
-    clear()
+    data = input("Would you like to see another users data? (Y/N)")
+    if data == "Y":
+        clear(0.5)
+        print_data = True
+    elif data == "N":
+        print("Okay, thank you for letting us steal your information:)")
+        print_data = False
+        break
+    else:
+        print("Please enter Y/N")
+        print_data = True
