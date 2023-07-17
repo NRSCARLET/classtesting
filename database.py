f2d68a31b0c9ac3a21090929f22d49491ce0fc27
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-07-17 23:33:09 +0000

    adding finishing touches and polishing

--- database.py
+++ database.py
@@ -1,21 +1,25 @@
+"""This file allows users to input their data and track information they've already inputted."""
 from os import system
 from time import sleep
 
-def clear():
+
+def clear():  
+    """This definition is to add a mechanism to clear the screan."""
     if name == 'nt':
         _ = system('cls')
     else:
         _ = system('clear')
 
-
+def wiblywobly():
+    print("-------------------------                                                               SCAMMER GOT SCAMMED LOSER                                                              ---------------------------")
 questions = True
 print_data = True
 user_desc = {}
 
-
-class Users:
-
+"""Represents user data to store it as a variable."""
+class Users:  
     def __init__(self, name, age, hobby):
+        """__init__ allows me to collect information as variables so I can call them back later in the code.""" 
         self.name = name
         self.age = age
         self.hobby = hobby
@@ -27,15 +31,11 @@ while questions is True:
     age = int(input("How old are you?\n"))
     hobby = input("What is your favourite hobby?\n").lower()
     user_desc[name] = Users(name, age, hobby)
-    print(
-        "Is there more user information you would like us to store in our database? (Y/N)"
-    )
+    print("Is there more user information you would like us to store in our database? (Y/N)")
     extra_user = input()
     while extra_user not in ("Y", "N"):
         print("Please enter Y/N")
-        print(
-            "Is there more user information you would like us to store in our database? (Y/N)"
-        )
+        print("Is there more user information you would like us to store in our database (Y/N)")
         extra_user = input()
 
     if extra_user == "Y":
@@ -50,11 +50,13 @@ while questions is True:
         break
 print(user_desc.keys())
 while print_data is True:
-    print("Who's user data would you like to see? (Input users name)")
+    print("Whos user data would you like to see? (Input users name)")
     u = input()
     u = u.replace("'", "")
     u = u.lower()
-    print(f"Your name is {u.title()}, You are {user_desc[u].age} years old, and your favourite hobby is {user_desc[u].hobby}")
+    print(
+        f"Your name is {u.title()}, You are {user_desc[u].age} years old, and your favourite hobby is {user_desc[u].hobby}"
+    )
     print("Would you like to see another users data? (Y/N)")
     data = input()
     if data == "Y":
@@ -62,7 +64,9 @@ while print_data is True:
     elif data == "N":
         print("Okay, thank you for letting us steal your information:)")
         print_data = False
-        break
+        sleep(0.5)
+        clear()
+        wiblywobly()
     else:
         print("Please enter Y/N")
         print("Would you like to see another users data? (Y/N)\n")
