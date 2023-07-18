d53fe76b4397e3b5a37f779be6b002ba6e4e85bc
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-07-18 00:34:57 +0000

    error catching for entering names to check data

--- database.py
+++ database.py
@@ -23,49 +23,29 @@ class Users:
         self.name = name
         self.age = age
         self.hobby = hobby
+def data_print():
+    while print_data is True:
+        print("Whos user data would you like to see? (Input users name)")
+        u = input()
+        u = u.replace("'", "")
+        u = u.lower()
+        try:
+            print(f"Your name is {u.title()}, You are {user_desc[u].age} years old, and your favourite hobby is {user_desc[u].hobby}")
+            user_num()
+        except KeyError:
+            print("Please enter a name you've put into our database")
+            sleep(1)
+            clear()
+            data_print()
 
-
-while questions is True:
-    print("Tell me about yourself")
-    name = input("What is your name?\n").lower()
-    age = int(input("How old are you?\n"))
-    hobby = input("What is your favourite hobby?\n").lower()
-    user_desc[name] = Users(name, age, hobby)
-    print("Is there more user information you would like us to store in our database? (Y/N)")
-    extra_user = input()
-    while extra_user not in ("Y", "N"):
-        print("Please enter Y/N")
-        print("Is there more user information you would like us to store in our database (Y/N)")
-        extra_user = input()
-
-    if extra_user == "Y":
-        print("Loading new user interface...")
-        number_of_users =+1
-        sleep(0.5)
-        clear()
-        questions = True
-    elif extra_user == "N":
-        print("Okay, thank you for inputting your data...")
-        sleep(0.7)
-        clear()
-        break
-print(user_desc.keys())
-while print_data is True:
-    print("Whos user data would you like to see? (Input users name)")
-    u = input()
-    u = u.replace("'", "")
-    u = u.lower()
-    if = user_desc.keys():
-    print(f"Your name is {u.title()}, You are {user_desc[u].age} years old, and your favourite hobby is {user_desc[u].hobby}"
-    )
+def user_num():
     if number_of_users >= 1:
         print("Would you like to see another users data? (Y/N)")
         data = input()
         if data == "Y":
-            print_data = True
+            data_print()
         elif data == "N":
             print("Okay, thank you for letting us steal your information:)")
-            print_data = False
             sleep(1)
             clear()
             wiblywobly()
@@ -74,8 +54,38 @@ while print_data is True:
             print("Would you like to see another users data? (Y/N)\n")
             data = input()
     else:
-        print_data = False
         print("Okay, thank you for letting us steal your information:)")
         sleep(1)
         clear()
         wiblywobly()
+
+
+while questions is True:
+    print("Tell me about yourself")
+    name = input("What is your name?\n").lower()
+    age = (input("How old are you?\n"))
+    try:
+        hobby = input("What is your favourite hobby?\n").lower()
+        user_desc[name] = Users(name, age, hobby)
+        print("Is there more user information you would like us to store in our database? (Y/N)")
+        extra_user = input()
+        while extra_user not in ("Y", "N"):
+            print("Please enter Y/N")
+            print("Is there more user information you would like us to store in our database (Y/N)")
+            extra_user = input()
+    
+        if extra_user == "Y":
+            print("Loading new user interface...")
+            number_of_users =+1
+            sleep(0.5)
+            clear()
+            questions = True
+        elif extra_user == "N":
+            print("Okay, thank you for inputting your data...")
+            sleep(0.7)
+            clear()
+            print(user_desc.keys())
+            data_print()
+    except ValueError:
+        print("Please enter a number")
+        questions = True
